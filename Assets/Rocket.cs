3b358a1e876a45c8570a25c247002625bd76787e
Jared Germano <jgermano@cadlearning.com>
2018-08-17 11:54:21 -0400

    To Lecture 57

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField]
+    float MainThrust = 200;
+
+    [SerializeField]
+    float RCSThrust = 50;
+
+    [SerializeField]
+    float ImpactDelay = 1;
+
     private Rigidbody rigidBody;
     private AudioSource audioSource;
+
+    [SerializeField]
+    GameObject launchPoint;
+
+    enum State {  Alive, Dying, Transcending }
+    State state = State.Alive;
+
     // Use this for initialization
     public void Start()
     {
@@ -17,15 +31,52 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state == State.Alive)
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Friendly":
+                    break;
+                case "Finish":
+                    state = State.Transcending;
+                    Invoke("LoadNextLevel", ImpactDelay);
+                    break;
+                default:
+                    state = State.Dying;
+                    audioSource.Stop();
+                    Invoke("LoadNextLevel", ImpactDelay);
+                    break;
+            }
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        switch(state)
+        {
+            case State.Dying:
+                SceneManager.LoadScene(0);
+                break;
+            case State.Transcending:
+                SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+                break;
+        }
     }
 
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * MainThrust * Time.deltaTime);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -43,11 +94,11 @@ public class Rocket : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * RCSThrust * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * RCSThrust * Time.deltaTime);
         }
 
         rigidBody.freezeRotation = false;
